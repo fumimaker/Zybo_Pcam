59f4509b3b29ff3e8d5d30a2409a673a3eb7c1b3
fumi <fumi01200924@gmail.com>
2020-01-22 00:17:53 +0900

    Update term.py

--- opencv/term.py
+++ opencv/term.py
@@ -1,8 +1,7 @@
 import cv2
 import numpy as np
-
+import serial
 cap = cv2.VideoCapture(0)
-#cap.set(cv2.CAP_PROP_FPS, 30)
 print(cap.get(cv2.CAP_PROP_FPS))
 
 tm = cv2.TickMeter()
@@ -10,6 +9,21 @@ tm.start()
 count = 0
 max_count = 10
 fps = 0
+
+def find_rect_of_target_color(image):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV_FULL)
+    h = hsv[:, :, 0]
+    s = hsv[:, :, 1]
+    mask = np.zeros(h.shape, dtype=np.uint8)
+
+    mask[(h==0) & (s==0)] = 128
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    rects = []
+    for contour in contours:
+        approx = cv2.convexHull(contour)
+        rect = cv2.boundingRect(approx)
+        rects.append(np.array(rect))
+    return rects
 
 while(1):
     # フレームを取得
@@ -23,7 +37,13 @@ while(1):
         count = 0
     cv2.putText(frame, 'FPS: {:.2f}'.format(fps),
                 (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), thickness=2)
-    cv2.imshow("frame", frame)
+    
+    
+    rects = find_rect_of_target_color(frame)
+    if len(rects) > 0:
+      rect = max(rects, key=(lambda x: x[2] * x[3]))
+      cv2.rectangle(frame, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), (0, 0, 255), thickness=2)
+    cv2.imshow('red', frame)
 
     count += 1
 
