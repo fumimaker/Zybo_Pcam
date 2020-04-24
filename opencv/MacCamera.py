9fcc70b78ab59b27e677fa8f889d30827fea98fc
fumi <fumi01200924@gmail.com>
2020-04-24 15:18:54 +0900

    macCamera

--- opencv/MacCamera.py
+++ opencv/MacCamera.py
@@ -1,13 +1,18 @@
+#!/Users/fumi/.pyenv/shims/python
+
 import numpy as np
 import cv2
+cap = cv2.VideoCapture(1)
+#cap.set(cv2.CAP_PROP_FPS, 30)
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FPS, 30)
 while(True):
+    
     # Capture frame-by-frame
     ret, frame = cap.read()
-
     # Display the resulting frame
+    qr = cv2.QRCodeDetector()
+    data, points, _ = qr.detectAndDecode(frame)
+    print(data, points)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
